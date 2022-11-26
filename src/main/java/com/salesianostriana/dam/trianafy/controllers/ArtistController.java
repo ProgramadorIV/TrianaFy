cdf89c1ad4f6a0e6c5acc571a1837f45e6beb841
Your Name <antoniojz1998@gmail.com>
2022-11-26 01:21:57 +0100

    Se finalizan todos los endpoints menos el delete por integridad referencial.

--- src/main/java/com/salesianostriana/dam/trianafy/controllers/ArtistController.java
+++ src/main/java/com/salesianostriana/dam/trianafy/controllers/ArtistController.java
@@ -1,7 +1,6 @@
 package com.salesianostriana.dam.trianafy.controllers;
 
 import com.salesianostriana.dam.trianafy.model.Artist;
-import com.salesianostriana.dam.trianafy.repos.ArtistRepository;
 import com.salesianostriana.dam.trianafy.service.ArtistService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -11,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/artist")
@@ -18,35 +18,64 @@ public class ArtistController {
 
     private final ArtistService artistService;
 
-    @PostMapping("/")
+    @PostMapping("/") //400
     public ResponseEntity<Artist> createArtist(@RequestBody Artist artist){
         return ResponseEntity
                 .status(HttpStatus.CREATED)
                 .body(artistService.add(artist));
     }
 
-    @GetMapping("/")
+    @GetMapping("/") //404
     public ResponseEntity<List<Artist>> getAllArtist(){
-        return ResponseEntity.ok(artistService.findAll());
+        if(artistService.findAll().isEmpty())
+            return ResponseEntity.notFound().build();
+        else{
+            return ResponseEntity.ok().body(artistService.findAll());
+        }
     }
 
-    @GetMapping("/{id}")
+
+    @GetMapping("/{id}") //404
     public ResponseEntity<Artist> getArtistById(@PathVariable Long id){
-        return ResponseEntity.of(artistService.findById(id));
+        List<Artist> lista = getAllArtist().getBody();
+
+        try {
+            for (Artist artist: lista) {
+                if(artist.getId() == id)
+                    return ResponseEntity.of(artistService.findById(id));
+            }
+        }
+        catch (NullPointerException e){
+            return ResponseEntity.notFound().build();
+        }
+
+        return ResponseEntity.notFound().build();
+
     }
 
-    @PutMapping("/{id}")
+    @PutMapping("/{id}") //404
     public ResponseEntity<Artist> updateArtist(@PathVariable Long id, @RequestBody Artist artist){
-        if(!artistService.findById(id).isEmpty())
+        if(artistService.findById(id).isPresent())
             return ResponseEntity.of(
                     artistService.findById(id)
                             .map(art -> {
-                                art.setName(art.getName());
-                                return Optional.of(artistService.edit(art));
+                                art.setName(artist.getName());
+                                return Optional.of(artistService.add(art));
                             }).orElse(Optional.empty())
             );
 
-        return ResponseEntity.badRequest().build();
+        return ResponseEntity.notFound().build();
 
+    }
+
+    //No va porq el artista esta asociado a las canciones.
+    @DeleteMapping("/{id}") //404
+    public ResponseEntity<?> deleteArtist(@PathVariable Long id){
+        if(!artistService.findById(id).isEmpty()){
+            artistService.deleteById(id);
+            return ResponseEntity.noContent().build();
+        }
+
+        return ResponseEntity.notFound().build();
     }
 }
